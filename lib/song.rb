f11d0e1c52dc87ce1200aac9970e879faad83c81
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-07-20 22:14:38 -0700

    completed

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+require "pry"
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << self.artist
+    @@genres << self.genre
+    # binding.pry
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+  #   binding.pry
+
+  def self.genre_count
+    @@genres.tally
+  end
+
+  def self.artist_count
+    @@artists.tally
+  end
+end
